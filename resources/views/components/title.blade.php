d9b7fed2a55f4a40dcc8954c09928a5d7ed878c1
anan <nannbt29@gmail.com>
2022-12-30 17:52:05 +0700

    title segment fixed

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,6 +1,6 @@
 <title>
-    @if (Request::segment(0) != null || Request::segment(1) != null || Request::segment(2) != null)
-        {{ config('app.name', 'Traffic Light System')." - ".Request::segment(0).Request::segment(2) }}
+    @if (Request::segment(3) != null)
+        {{ config('app.name', 'Traffic Light System')." - ".Request::segment(3) }}
     @else
         {{ config('app.name', 'Traffic Light System') }}
     @endif
